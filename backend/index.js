bcc9f6d6b2455617badca02e239a5fcf855a04f3
arrudacaio <caio.arruda@ccc.ufcg.edu.br>
2020-01-14 17:25:06 -0300

    add métodos http

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,15 @@ const express = require("express");
 
 const app = express();
 
+/*
+    Criação de Rotas utilizando diferentes métodos:
+    ---MÉTODOS HTTP: ------------------------
+        get: buscar/receber  informações
+        post: criar alguma informação
+        put: editar um recurso da minha aplicação
+        delete: deletar 
+
+*/
 app.get("/", (request, response) => {
   return response.json({ message: "Hello Caio" });
 });
