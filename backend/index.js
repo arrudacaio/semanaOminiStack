9c31952658f22a8c02d750ab571cafd30696b853
arrudacaio <caio.arruda@ccc.ufcg.edu.br>
2020-01-15 04:20:03 -0300

    att

--- backend/index.js
+++ backend/index.js
@@ -10,9 +10,17 @@ const app = express();
         put: editar um recurso da minha aplicação
         delete: deletar 
 
+    --- Tipos de paramêtros dentro do Express-----
+    Query Params: (Utilizados apenas no método get! São usados para filtros, ordenação, paginação...)
+    pode ser acessado através de request.query
+
+    Route Params:
+    Body:
+
 */
 app.get("/", (request, response) => {
-  return response.json({ message: "Hello Caio" });
+  console.log(request.query);
+  return response.json({ message: "Hello OmniStack" });
 });
 
 app.listen(3333);
