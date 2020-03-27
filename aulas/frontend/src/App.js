755d03bf37afc503ce6356985401796d660f5f07
arrudacaio <caio.arruda@ccc.ufcg.edu.br>
2020-03-27 03:23:43 -0300

    aplicando os conceitos básicos do react

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -1,22 +1,15 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
+import Header from "./Header";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  function increment() {
+    setCounter(counter + 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header>Counter: {counter}</Header>
+      <button onClick={increment}>Incrementar</button>
     </div>
   );
 }
