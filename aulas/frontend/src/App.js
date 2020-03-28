5cdbb70b56e9fe8e25d75929fa0587dc69ccdeab
arrudacaio <caio.arruda@ccc.ufcg.edu.br>
2020-03-28 04:29:48 -0300

    add new features

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -1,17 +1,9 @@
-import React, { useState } from "react";
-import Header from "./Header";
+import React from "react";
 
-function App() {
-  const [counter, setCounter] = useState(0);
-  function increment() {
-    setCounter(counter + 1);
-  }
-  return (
-    <div>
-      <Header>Counter: {counter}</Header>
-      <button onClick={increment}>Incrementar</button>
-    </div>
-  );
+import "./global.css";
+
+import Routes from "./routes";
+
+export default function App() {
+  return <Routes />;
 }
-
-export default App;
